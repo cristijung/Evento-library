3c12635eca2aec9b1c00e55389354327ec88eaaf
Cristina Jung <cristijung@gmail.com>
2023-08-07 09:36:27 -0300

    image function

--- app/src/main/java/com/example/eventolivraria/MainActivity.kt
+++ app/src/main/java/com/example/eventolivraria/MainActivity.kt
@@ -3,12 +3,14 @@ package com.example.eventolivraria
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.sp
@@ -33,8 +35,13 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun LibraryGreetingWithImage(message: String, from:String, modifier: Modifier = Modifier) {
-
+    val image = painterResource(R.drawable.androidlibrary)
+    Image(
+        painter = image,
+        contentDescription = null
+    )
 }
+
 @Composable
 fun GreetingText(message: String, modifier: Modifier = Modifier) {
     Text(
