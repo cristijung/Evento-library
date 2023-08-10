7acc8a6b3ebd6047576c2f2a310db8abecfbb52d
Cristina Jung <cristijung@gmail.com>
2023-08-10 13:09:37 -0300

    edição de texto

--- app/src/main/java/com/example/eventolivraria/MainActivity.kt
+++ app/src/main/java/com/example/eventolivraria/MainActivity.kt
@@ -4,15 +4,19 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.eventolivraria.ui.theme.EventoLivrariaTheme
 
@@ -26,7 +30,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-
+                    LibraryGreetingWithImage(message = "Oi", from = "Livraria")
                 }
             }
         }
@@ -36,25 +40,44 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun LibraryGreetingWithImage(message: String, from:String, modifier: Modifier = Modifier) {
     val image = painterResource(R.drawable.androidlibrary)
-    Image(
-        painter = image,
-        contentDescription = null
-    )
+    Box {
+        Image(
+            painter = image,
+            contentDescription = null
+        )
+        GreetingText(
+            message = message,
+            from = from,
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(8.dp)
+        )
+    }
 }
 
 @Composable
-fun GreetingText(message: String, modifier: Modifier = Modifier) {
+fun GreetingText(message: String, from: String, modifier: Modifier = Modifier) {
     Text(
         text = message,
-        fontSize = 100.sp,
+        fontSize = 90.sp,
         lineHeight = 116.sp,
-        textAlign = TextAlign.Center
+        textAlign = TextAlign.Center,
+        color = Color.White,
+
+    )
+    Text(
+        text = from,
+        fontSize = 36.sp,
+        modifier = Modifier
+            .padding(top = 16.dp)
+            .padding(end = 16.dp)
+
     )
 }
 @Preview(showBackground = true)
 @Composable
 fun LibraryEventPreview() {
     EventoLivrariaTheme {
-        GreetingText( message = " Seja Bem Vindo a The Book House")
+        LibraryGreetingWithImage("Seja Bem Vindo!", "- The House of Book" )
     }
 }
